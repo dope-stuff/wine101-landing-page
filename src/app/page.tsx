ba6884c9aa3b30ebe92c15c8ca692cfbdc47067f
Lisardgy <key.cmdark@gmail.com>
2024-06-05 19:23:21 +0700

    feat: update favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
-import Head from "next/head";
 import LandingPage from "../../components/landing";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <Head>
-        <link
-          rel="shortcut icon"
-          type="image/x-icon"
-          href="/wine101-landing-page/logo.ico"
-        />
-      </Head>
+      <link
+        rel="shortcut icon"
+        type="image/x-icon"
+        href="logo.ico"
+      />
       <LandingPage />
     </main>
   );
